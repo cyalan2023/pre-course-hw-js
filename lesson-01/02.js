2a5dcc2e8ac970c5f1965f4cfd7d27a6045400b1
Alex <cyalan2019@gmail.com>
2023-11-08 20:35:01 +0500

    lesson-01/4

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,3 +1,3 @@
 let number1 = Math.floor(Math.random() * 100);
 let result = number1 % 2 ? 'Это нечетное число' : 'Это четное число';
-console.log(number1, `${result}`)
+console.log(number1, result)
