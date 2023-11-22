ad83908eeb9463fbf4939eae0e5c73c0b4dd4fbd
Alex <cyalan2019@gmail.com>
2023-11-22 18:46:23 +0500

    start

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,6 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(el => el ==="черный" || el === "желтый" || el === "красный").join("-")
+}
+console.log(createColorString());
